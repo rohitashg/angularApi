823df5bae2ec0ffb5bed40b30c9781fe9e1a22ce
Anthony Petti <apetti@tk.co>
2017-06-07 16:17:08 +0530

    dadjd

--- db.php
+++ db.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+/*header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header('Access-Control-Allow-Credentials: true');
+*/
 $con = mysqli_connect("localhost","root","kiwitech","testAngularApi");
 
 // Check connection
--- signup.php
+++ signup.php
@@ -1,23 +1,31 @@
 <?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header('Access-Control-Allow-Credentials: true');
+error_reporting(E_ALL);
 require_once('db.php');
-if(!empty($_POST)){
-	//echo '<pre>';print_r($_POST);die('here');
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$subject = $_POST['subject'];
-	$message = $_POST['message'];
-	$sql = "INSERT INTO users (name,email,subject,about_your_self) 
-	VALUES ('$name','$email','$subject','$message')";
+$request_body = file_get_contents('php://input');
+if(!empty($request_body) && $request_body !=''){
+	//echo '<pre>';print_r($request_body);die;
+	$data = json_decode($request_body);
+	$name = $data->name;
+	$email = $data->email;
+	$subject = $data->subject;
+	$message = $data->message;
+	$sql = "INSERT INTO users (name,email,subject,about_your_self) VALUES ('$name','$email','$subject','$message')";
 	$res = mysqli_query($con,$sql);
-	if($res){
+
+	if($res==1){
 		$data['success'] = 'ok';
 		$data['msg'] = 'User registered successfully.';
+		echo '<pre>';print_r($data);die;
 	}else{
 		$data['success'] = 'Failed';
 		$data['msg'] = 'User is not registered.';
+		echo '<pre>';print_r($data);die;
 	}
 	mysqli_close($con);
-	echo json_encode($data);die;
+	return json_encode($data);die;
 }else{
 	echo 'else come here';die;
 }
